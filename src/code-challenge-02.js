48e1703342b25bc2acec9c9b67016dce24377fee
Pete Hamrick <pete.hamrick@gmail.com>
2021-08-11 09:23:12 -0700

    fiveAndGreaterOnly passing test

---
+++ src/code-challenge-02.js
@@ -0,0 +1,4 @@
+export function fiveAndGreaterOnly(arr){
+    const fivePlus = arr.filter(num => num >= 5);
+    return fivePlus;
+}
---
+++ src/code-challenge-02.test.js
@@ -0,0 +1,7 @@
+import { fiveAndGreaterOnly } from './code-challenge-02.js';
+
+test('Five and greater only', () => {
+  const input = [3, 6, 8, 2]; // arrange
+  const output = fiveAndGreaterOnly(input); // act
+  expect(output).toEqual([6, 8]); // assert
+});
