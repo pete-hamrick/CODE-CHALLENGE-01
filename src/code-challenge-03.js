0131cdf116757475b07d3b2732189e7cc1505542
Pete Hamrick <pete.hamrick@gmail.com>
2021-08-16 16:49:33 -0700

    wrote failing test for sortedKeys

--- src/code-challenge-03.js
+++ src/code-challenge-03.js
@@ -2,3 +2,7 @@ export function capitalizeObjectKeys(obj){
     const arr = Object.keys(obj);
     return arr.map(str => str.toUpperCase())
 }
+
+export function sortedKeys(obj) {
+
+}
--- src/code-challenge-03.test.js
+++ src/code-challenge-03.test.js
@@ -1,7 +1,13 @@
-import { capitalizeObjectKeys } from './code-challenge-03.js';
+import { capitalizeObjectKeys, sortedKeys } from './code-challenge-03.js';
 
 test('it capitalizes the keys', () => {
   const dataObject = { name: 'Angelina Jolie', age: 80 }; //arrange
   const output = capitalizeObjectKeys(dataObject); //act
   expect(output).toEqual(['NAME', 'AGE']); //assert
+});
+
+test('it sorts the keys by shortest to longest', () => {
+  const dataObject = { name: 'Spot', age: 3, isDog: true, friends: ['Rover', 'Steve', 'Fluffy'] }; //arrange
+  const output = sortedKeys(dataObject); //act
+  expect(output).toEqual(['age', 'name', 'isDog', 'friends']); //assert
 });
