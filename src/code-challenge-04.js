7b043e17e93ab3f6517b46982dc406a779701a54
Pete Hamrick <pete.hamrick@gmail.com>
2021-08-18 17:14:36 -0700

    passing ugly totalCharacters function and test

--- src/code-challenge-04.js
+++ src/code-challenge-04.js
@@ -8,3 +8,17 @@ export function updateNumbers(obj) {
     const arr = Object.entries(obj)
     return arr.map(item => `${item[0]}: ${item[1]}`)
 }
+
+export function totalCharacters(arr) {
+    const names = Object.values(arr).map(item => item.name)
+    const spouse = (Object.values(arr).map(item => item.spouse)).filter(function (thing) {return thing != null;})
+    const children = (Object.values(arr).map(item => item.children))
+    const kids = children.flat(1)
+    console.log(names.length + spouse.length + kids.length)
+    return names.length + spouse.length + kids.length
+}
+
+// export function hasChildrenEntries(arr, character) {
+//     const array = Object.entries(arr);
+
+// } 
--- src/code-challenge-04.test.js
+++ src/code-challenge-04.test.js
@@ -1,4 +1,4 @@
-import { getHouses, updateNumbers } from './code-challenge-04.js';
+import { getHouses, hasChildrenEntries, totalCharacters, updateNumbers } from './code-challenge-04.js';
 test('it gets the names of the houses', () => {
   const characters = [
     {
@@ -53,3 +53,102 @@ test('it returns an array of transformed key value pairs into strings', () => {
   const output = updateNumbers(characters); //act
   expect(output).toEqual(['Grace Hopper: 222-303-5938','Ada Lovelace: 222-349-9842','Alan Turing: 222-853-5933']); //assert
 });
+
+test('gets the total characters', () => {
+  const characters = [
+    {
+      name: 'Eddard',
+      spouse: 'Catelyn',
+      children: ['Robb', 'Sansa', 'Arya', 'Bran', 'Rickon'],
+      house: 'Stark'
+    },
+    {
+      name: 'Jon A.',
+      spouse: 'Lysa',
+      children: ['Robin'],
+      house: 'Arryn'
+    },
+    {
+      name: 'Cersei',
+      spouse: 'Robert',
+      children: ['Joffrey', 'Myrcella', 'Tommen'],
+      house: 'Lannister'
+    },
+    {
+      name: 'Daenarys',
+      spouse: 'Khal Drogo',
+      children: ['Drogon', 'Rhaegal', 'Viserion'],
+      house: 'Targaryen'
+    },
+    {
+      name: 'Mace',
+      spouse: 'Alerie',
+      children: ['Margaery', 'Loras'],
+      house: 'Tyrell'
+    },
+    {
+      name: 'Euron',
+      spouse: null,
+      children: [],
+      house: 'Greyjoy'
+    },
+    {
+      name: 'Jon S.',
+      spouse: null,
+      children: [],
+      house: 'Snow'
+    }
+  ]; //arrange
+  const output = totalCharacters(characters); //act
+  expect(output).toEqual(26); //assert
+});
+
+// test('does the character have children?', () => {
+//     const character = 'Eddard';
+//     const characters = [
+//     {
+//       name: 'Eddard',
+//       spouse: 'Catelyn',
+//       children: ['Robb', 'Sansa', 'Arya', 'Bran', 'Rickon'],
+//       house: 'Stark'
+//     },
+//     {
+//       name: 'Jon A.',
+//       spouse: 'Lysa',
+//       children: ['Robin'],
+//       house: 'Arryn'
+//     },
+//     {
+//       name: 'Cersei',
+//       spouse: 'Robert',
+//       children: ['Joffrey', 'Myrcella', 'Tommen'],
+//       house: 'Lannister'
+//     },
+//     {
+//       name: 'Daenarys',
+//       spouse: 'Khal Drogo',
+//       children: ['Drogon', 'Rhaegal', 'Viserion'],
+//       house: 'Targaryen'
+//     },
+//     {
+//       name: 'Mace',
+//       spouse: 'Alerie',
+//       children: ['Margaery', 'Loras'],
+//       house: 'Tyrell'
+//     },
+//     {
+//       name: 'Euron',
+//       spouse: null,
+//       children: [],
+//       house: 'Greyjoy'
+//     },
+//     {
+//       name: 'Jon S.',
+//       spouse: null,
+//       children: [],
+//       house: 'Snow'
+//     }
+//   ]; //arrange
+//   const output = hasChildrenEntries(characters, character); //act
+//   expect(output).toEqual(true); //assert
+// });
