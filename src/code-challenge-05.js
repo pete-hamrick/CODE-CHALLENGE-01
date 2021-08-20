a41d5ac466264ec53d0518ad1e8cab91beccb7c3
Pete Hamrick <pete.hamrick@gmail.com>
2021-08-19 17:46:44 -0700

    passing howMuchPencil test

---
+++ src/code-challenge-05.js
@@ -0,0 +1,7 @@
+export function howMuchPencil(str) {
+    let pencil = [];
+    for (let i = 0; i <= str.length; i++) {
+        pencil.push(str.slice(i))
+    }
+    return pencil
+}
---
+++ src/code-challenge-05.test.js
@@ -0,0 +1,8 @@
+import { howMuchPencil } from './code-challenge-05';
+
+ test('It should return a list of shortening words', () => {
+    expect(howMuchPencil('Welcome')).toStrictEqual(['Welcome', 'elcome', 'lcome', 'come', 'ome', 'me', 'e', '']);
+    expect(howMuchPencil('Welcome').length).toStrictEqual(8);
+    expect(howMuchPencil('')).toStrictEqual(['']);
+    expect(howMuchPencil('abc')).toStrictEqual(['abc', 'bc', 'c', '']);
+  });
