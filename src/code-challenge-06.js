67dba0b3017eda23a5512a5bd5345381ff39e679
Pete Hamrick <pete.hamrick@gmail.com>
2021-08-25 18:54:02 -0700

    passing grandTotal test, big assist to paul

--- src/code-challenge-06.js
+++ src/code-challenge-06.js
@@ -10,3 +10,25 @@ export function totalSum(matrix) {
     let arr = matrix.flat(2)
     return arr.reduce((a, b) => a + b, 0)
 }
+
+
+export function grandTotal(stores){
+    let sum = []
+    for (let store of stores) {
+      for (let i in store) {
+        if (sum[i]) {
+            sum[i] += store[i]
+        } else {
+            sum[i] = store[i]
+        }
+      }
+    } 
+    return sum;
+}
+
+export function salesData(hours, data){
+    // const data = [];
+    // for (let hour of hours) {
+        
+    // }
+}
